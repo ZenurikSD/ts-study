593f0eab86f5e20ec582400350972539956ccd55
ZenurikSD <alissonsilva498@outlook.com.br>
2024-09-17 19:38:15 -0300

    Reverse string exercise

---
+++ src/playground/exercises.ts
@@ -0,0 +1,14 @@
+var string = "Sleeping in the cold below";
+
+function reverseString(string: string) : string{
+
+    var reversedString : string;
+
+    for(let i=string.length; i>0; i++){
+
+        
+    }
+
+
+    return reversedString;
+}
