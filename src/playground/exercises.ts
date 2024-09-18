150fca3509755d924e72a85be09953ce4f038e7e
ZenurikSD <alissonsilva498@outlook.com.br>
2024-09-18 18:54:27 -0300

    Array operations

--- src/playground/exercises.ts
+++ src/playground/exercises.ts
@@ -1,14 +1,27 @@
-var string = "Sleeping in the cold below";
 
+//String manipulation
 function reverseString(string: string) : string{
 
-    var reversedString : string;
+    var reversedString = '';
 
-    for(let i=string.length; i>0; i++){
-
+    for(let i=string.length-1; i>=0; i--){
+        reversedString += string[i]
         
     }
-
-
     return reversedString;
 }
+console.log(reverseString('Hello'));
+
+
+// Array operations
+function findMax(array: number[]): number{
+    var max = 0;
+    for (let number of array){
+        if (number > max){
+            max = number;
+        }
+    }
+
+    return max;
+}
+console.log(findMax([0, 3, 2, 6, 123, 124, 12.5, 1]));
